4f8aa8233d0a36f4f0c7e3c18562c3dce0ea7bb0
Sheayun Lee <sheayun@gmail.com>
2026-04-27 15:16:56 +0900

    Add a sample using namespaces

---
+++ samples/Ch11/dtnamespace/dtime.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <cstdlib>
+using std::istream;
+using std::ostream;
+using std::cerr;
+using std::endl;
+#include "dtime.h"
+
+namespace  { // Specifies an unnamed namespace
+
+int digitToInt(char c)
+{
+    return static_cast<int> (c) - static_cast<int> ('0');
+}
+
+void readHour(istream &ins, int &theHour)
+{
+    char c1, c2;
+    ins >> c1 >> c2;
+    if (!(isdigit(c1) &&  (isdigit(c2) || c2 == ':'))) {
+        cerr << "Illegal hour input to readHour\n";
+        exit(1);
+    }
+    if (isdigit(c1) && c2 == ':') {
+        theHour = digitToInt(c1);
+    }
+    else { // (isdigit(c1) && isdigit(c2))
+        theHour = digitToInt(c1) * 10 + digitToInt(c2);
+        ins >> c2; // discard ':'
+        if (c2 != ':') {
+            cerr << "Illegal hour input to readHour\n";
+            exit(1);
+        }
+    }
+    if (theHour == 24) {
+        theHour = 0;
+    }
+    if (theHour < 0 || theHour > 23) {
+        cerr << "Illegal hour input to readHour\n";
+        exit(1);
+    }
+}
+
+void readMinute(istream &ins, int &theMinute)
+{
+    char c1, c2;
+    ins >> c1 >> c2;
+    if (!(isdigit(c1) && isdigit(c2))) {
+        cerr << "Illegal minute input to readMinute\n";
+        exit(1);
+    }
+    theMinute = digitToInt(c1) * 10 + digitToInt(c2);
+    if (theMinute < 0 || theMinute > 59) {
+        cerr << "Illegal minute input to readMinute\n";
+        exit(1);
+    }
+}
+
+} // end unnamed namespace
+
+namespace DTimeCAS1102 {
+
+DigitalTime::DigitalTime(int theHour, int theMinute)
+{
+    if (theHour < 0 || theHour > 23 || theMinute < 0 || theMinute > 59) {
+        cerr << "Illegal argument to DigitalTime constructor." << endl;
+        exit(1);
+    }
+    else {
+        hour = theHour;
+        minute = theMinute;
+    }
+    if (hour == 24) {
+        hour = 0;
+    }
+}
+
+DigitalTime::DigitalTime()
+{
+    hour = 0;
+    minute = 0;
+}
+
+int DigitalTime::getHour() const
+{
+    return hour;
+}
+
+int DigitalTime::getMinute() const
+{
+    return minute;
+}
+
+void DigitalTime::advance(int minutesAdded)
+{
+    int grossMinutes = minute + minutesAdded;
+    minute = grossMinutes % 60;
+    int hourAdjustment = grossMinutes / 60;
+    hour = (hour + hourAdjustment) % 24;
+}
+
+void DigitalTime::advance(int hoursAdded, int minutesAdded)
+{
+    hour = (hour + hoursAdded) % 24;
+    advance(minutesAdded);
+}
+
+bool operator ==(const DigitalTime &time1, const DigitalTime &time2)
+{
+    return (time1.hour == time2.hour && time1.minute == time2.minute);
+}
+
+ostream &operator <<(ostream &outs, const DigitalTime &theObject)
+{
+    if (theObject.hour < 10) {
+        outs << '0';
+    }
+    outs << theObject.hour << ':';
+    if (theObject.minute < 10) {
+        outs << '0';
+    }
+    outs << theObject.minute;
+    return outs;
+}
+
+istream &operator >>(istream &ins, DigitalTime &theObject)
+{
+    readHour(ins, theObject.hour);
+    readMinute(ins, theObject.minute);
+    return ins;
+}
+
+} // end namespace DTimeCAS1102
---
+++ samples/Ch11/dtnamespace/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "dtime.h"
+
+void readHour(int &theHour)
+{
+    using std::cout;
+    using std::cin;
+
+    cout << "Let's play a time game.\n"
+         << "Let's pretend the hour has just changed.\n"
+         << "You may write midnight as either 0 or 24,\n"
+         << "but, I will always write it as 0.\n"
+         << "Enter the hour as a number (0 to 24): ";
+    cin >> theHour;
+}
+
+int main()
+{
+    using std::cout;
+    using std::cin;
+    using std::cerr;
+    using std::endl;
+
+    using DTimeCAS1102::DigitalTime;
+    int theHour;
+    readHour(theHour);
+    DigitalTime clock(theHour, 0), oldClock;
+
+    oldClock = clock;
+    clock.advance(15);
+    if (clock == oldClock) {
+        cerr << "Something is wrong.\n";
+    }
+    cout << "You entered " << oldClock << endl;
+    cout << "15 minutes later the time will be " << clock << endl;
+
+    clock.advance(2, 15);
+    cout << "2 hours and 15 minutes after that\n"
+         << "the time will be " << clock << endl;
+
+    return 0;
+}
