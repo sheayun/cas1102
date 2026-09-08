2bbf02e867d348822caa8d8bf9f6725018a82db7
Sheayun Lee <sheayun@gmail.com>
2026-09-08 10:27:16 +0900

    Add an indicative comment

--- samples/Ch02/enum.cpp
+++ samples/Ch02/enum.cpp
@@ -7,6 +7,7 @@ int main()
     Days d1 = Days::Mon;
     Days d2 = Days::Fri;
 
+    // cout << "d1 - " << d1 << endl; // Compile-time error: no match for ‘operator<<’ (operand types are ‘std::ostream {aka std::basic_ostream<char>}’ and ‘Days’)
+
     cout << "d1 - " << static_cast<int> (d1) << endl << "d2 - " << static_cast<int> (d2) << endl;
 }
-
